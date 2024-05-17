058584b5965289961daced9303514784b3cfffa8
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-17 15:34:35 +0200

    feat: add data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,12 @@
 </head>
 <body>
     <div class="container" style="margin: 0 auto; max-width: fit-content">
+        <h2 style="text-align: center;">
+            @foreach ($teachers as $teacher)
+                Ciao {{ $teacher }},&nbsp; <!-- ...perché gli spazi finali vengono trimmati ad ogni salvataggio - e non uso Prettier!-->
+            @endforeach
+            funziona!
+        </h2>
         <img style="height: 90vh" src="https://miro.medium.com/v2/resize:fit:998/format:webp/1*5djtVTN9PhSUwM97WT8-TA.jpeg" alt="">
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        "teachers" => [
+            "Alessandro",
+            "Fabio",
+            "Michele"
+        ]
+    ];
+    return view('home', $data);
 });
