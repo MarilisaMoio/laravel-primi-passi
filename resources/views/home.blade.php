cf223a8b34ca87b083fdf84eced604e3dec3edd5
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-17 15:16:11 +0200

    feat: add hello world - kind of

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,8 @@
     <title>Hello Laravel</title>
 </head>
 <body>
-
+    <div class="container" style="margin: 0 auto; max-width: fit-content">
+        <img style="height: 90vh" src="https://miro.medium.com/v2/resize:fit:998/format:webp/1*5djtVTN9PhSUwM97WT8-TA.jpeg" alt="">
+    </div>
 </body>
 </html>
